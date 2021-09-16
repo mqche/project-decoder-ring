f33d7a3cae7e2d3e0dff3f11820886d08dc4a27d
Minh Che <mche135@gmail.com>
2021-09-15 23:52:32 -0400

    caesar fix

--- src/caesar.js
+++ src/caesar.js
@@ -10,50 +10,50 @@ function caesar(input, shift, encode = true) {
  return (!encode) ? converter(lowerCase, -shift) : converter(lowerCase, shift);
 }
     // REFACTORED FUNC BUT NEEDS QUALIFIED TESTING
-function converter(input, shift) {
-  let encoded = [];
-    for (let i = 0; i < input.length; i++) {
-      let found = false;
-      for (let j = 0; j < arrOfLetters.length; j++) {
-          if (shift < 0 && input[i] === arrOfLetters[j]) {
-            let shifted = ((shift + j) + 26) % 26; 
-            encoded.push((arrOfLetters[shifted]))
-            found = true; 
-          } else (input[i] === arrOfLetters[j]) {
-          let shifted = (shift + j) % 26; 
-          encoded.push((arrOfLetters[shifted]))
-          found = true;  
-        }
-      } if (!found) {
-        encoded.push(input[i]); 
-      }}
-    return encoded.join("");  
-  }
-
-        // WORKING FUNC
 // function converter(input, shift) {
 //   let encoded = [];
 //     for (let i = 0; i < input.length; i++) {
 //       let found = false;
 //       for (let j = 0; j < arrOfLetters.length; j++) {
-//         if (shift < 0) {
-//           if (input[i] === arrOfLetters[j]) {
+//           if (shift < 0 && input[i] === arrOfLetters[j]) {
 //             let shifted = ((shift + j) + 26) % 26; 
 //             encoded.push((arrOfLetters[shifted]))
 //             found = true; 
-//           }
-//         } 
-//         else (input[i] === arrOfLetters[j]) {
+//           } else (input[i] === arrOfLetters[j]) {
 //           let shifted = (shift + j) % 26; 
 //           encoded.push((arrOfLetters[shifted]))
 //           found = true;  
 //         }
-//       } 
-//       if (!found) {
+//       } if (!found) {
 //         encoded.push(input[i]); 
 //       }}
 //     return encoded.join("");  
 //   }
+
+        // WORKING FUNC
+function converter(input, shift) {
+  let encoded = [];
+    for (let i = 0; i < input.length; i++) {
+      let found = false;
+      for (let j = 0; j < arrOfLetters.length; j++) {
+        if (shift < 0) {
+          if (input[i] === arrOfLetters[j]) {
+            let shifted = ((shift + j) + 26) % 26; 
+            encoded.push((arrOfLetters[shifted]))
+            found = true; 
+          }
+        } 
+        else (input[i] === arrOfLetters[j]) {
+          let shifted = (shift + j) % 26; 
+          encoded.push((arrOfLetters[shifted]))
+          found = true;  
+        }
+      } 
+      if (!found) {
+        encoded.push(input[i]); 
+      }}
+    return encoded.join("");  
+  }
 
   return {
     converter, caesar
